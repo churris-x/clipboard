3a13be8ec89c8b461b869bcb1ee08fd5a1db606b
Francisco Altimari <francisco@altimari.nl>
2023-06-29 18:47:12 -0300

    Remove react boilerplate

--- src/App.js
+++ src/App.js
@@ -3,20 +3,10 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+      <h1>Clipboard</h1>
+      <div className=''> </div>
       </header>
     </div>
   );
